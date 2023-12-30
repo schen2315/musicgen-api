71641e47ebc24b743d29b80d6fbc84b00820b8fa
Song Chen <schen2315@bths.edu>
2023-12-30 06:36:43 +0000

    server

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const express = require('express')
+const app = express()
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function makeMusic() {
-  const command = 'python melody.py --text "lofi girl" --outfile_name "lofi_girl"'
+async function makeMusic(text) {
+  const command = `python melody.py --text "${text}" --outfile_name "outputs/new_request"`
   const { stdout, stderr } = await exec(command)
 
-  console.log('stdout:', stdout);
-  console.error('stderr:', stderr);
+  //console.log('stdout:', stdout);
+  //console.error('stderr:', stderr);
 }
 
-makeMusic()
+//makeMusic()
+
+PORT = 3000
+
+app.use(express.static('outputs'))
+
+app.get('/api/musicgen', async (req, res) => {
+  console.log(`parmas text=${req.query.text}`)
+  await makeMusic(req.query.text)
+  res.send("Your song completed!")
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT=${PORT}`))
